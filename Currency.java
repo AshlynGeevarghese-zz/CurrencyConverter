78eb7126c8b0d3833bea7a438314e9fd705c0567
AshlynGeevarghese <71656492+AshlynGeevarghese@users.noreply.github.com>
2020-12-25 17:45:29 -0500

    Add files via upload

---
+++ Currency.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Currency{
+	public static void main (String[] args){
+		CurrencyData currency1 = new CurrencyData();
+	System.out.println("Please enter a number");
+	}
+}
